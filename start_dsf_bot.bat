5c3027877b06b95a09acc4a91b779f2867a11201
Adam Seidman <adamjseidman@gmail.com>
2020-07-19 14:20:41 -0400

    Update .bat to Recognize other instances

--- start_dsf_bot.bat
+++ start_dsf_bot.bat
@@ -1,4 +1,27 @@
-@echo off
-echo Intializing...
-node .
-pause
+@ECHO OFF
+
+IF EXIST temp.txt DEL /F temp.txt
+TASKLIST /NH /FI "WINDOWTITLE EQ DSF Robot" > temp.txt
+SET /P titlequery=<temp.txt
+IF NOT "%titlequery%" == "INFO: No tasks are running which match the specified criteria." (GOTO HasInst)
+
+:CreateServer
+ECHO Intializing...
+TITLE DSF Robot
+NODE .
+PAUSE
+GOTO Cleanup
+
+:HasInst
+ECHO There is already an instance of the bot running.
+ECHO Please exit now if you wouldn't like to replace that instance.
+ECHO.
+PAUSE
+
+TASKKILL /F /FI "WINDOWTITLE EQ DSF Robot"
+PAUSE
+CLS
+GOTO CreateServer
+
+:Cleanup
+IF EXIST temp.txt DEL /F temp.txt
