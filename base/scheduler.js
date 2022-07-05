e093a344bf2b09fcc421f5a0b75a611e63cca949
Adam <adamjseidman@gmail.com>
2022-07-05 17:49:21 -0400

    Comment scheduler.js

--- base/scheduler.js
+++ base/scheduler.js
@@ -1,14 +1,27 @@
+/**
+ * Author: Adam Seidman
+ * 
+ * Handles sending out daily stupid facts
+ * 
+ * Exports:
+ *     scheduleDailyChannels- Schedule fact
+ *     addDailyChannel- Add channel to list of dailies
+ *     removeDailyChannel- Remove channel from list of dailies
+ */
+
 const serverHandler = require('../db/handlers/server-info')
 const schedule = require('node-schedule')
 const utils = require('./facts')
 
 var dailyChannels = undefined
 
+// Schedule all daily channels
 var schedDailyChannels = function (clientChannels) {
     if (dailyChannels !== undefined) return
     dailyChannels = []
-    serverHandler.getDailyChannelsDB(clientChannels, dailyChannels)
-    schedule.scheduleJob({hour: 18, minute: 13}, async () => {
+    serverHandler.getDailyChannelsDB(clientChannels, dailyChannels) // Get channels
+    schedule.scheduleJob({hour: 18, minute: 13}, async () => { // 6:13 PM
+        // Send out fact at scheduled time
         let fact = utils.getRandomFact()
         dailyChannels.forEach(channel => {
             channel.send(`It's time for the fact of the day!\nAre you ready? Here it is:\n${fact}`)
@@ -16,17 +29,21 @@ var schedDailyChannels = function (clientChannels) {
     })
 }
 
+// Add a channel to list of daily channels
 var addDailyChannel = function (channel) {
     if (dailyChannels.find(item => {
         return channel.id === item.id
     }) !== undefined) {
+        // Channel already in dailyChannels list
         channel.send('Channel already set up.')
     } else {
+        // Add to array
         serverHandler.addDailyChannelDB(channel)
         dailyChannels.push(channel)
     }
 }
 
+// Remove a channel from list of daily channels
 var removeDailyChannel = function (channel) {
     var index = 0
     const foundChannel = dailyChannels.find((item, i) => {
@@ -37,9 +54,11 @@ var removeDailyChannel = function (channel) {
         return false
     }) !== undefined
     if (foundChannel) {
+        // Remove from database
         serverHandler.removeDailyChannelDB(channel)
         dailyChannels = dailyChannels.slice(0, index).concat(dailyChannels.slice(index + 1))
     } else {
+        // Was never set up
         channel.send('Channel not set up for daily stupid facts.')
     }
 }
