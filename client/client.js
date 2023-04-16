6555118b998147de569f4143eb86595c3ff0a9b1
Adam Seidman <adamjseidman@gmail.com>
2023-04-15 23:07:53 -0400

    Added Client Intents, and "messageCreate"
    Also updated likelihood of "shut up" message. "message" was changed to "messageCreate" because of dumb design decisions by Discord.

--- client/client.js
+++ client/client.js
@@ -13,7 +13,11 @@ const { messageHandlers  }= require('../base/messages')
 const itemHandler = require('../db/handlers/random-items')
 const setupWebServers = require('../base/web').setupWebServers
 
-const bot = new Discord.Client()
+// Bot Intentions
+const myIntents = ['Guilds', 'GuildVoiceStates', 'GuildMessages', 'DirectMessages',
+    'MessageContent', 'GuildScheduledEvents']
+
+const bot = new Discord.Client({ intents: myIntents })
 bot.login(token) // Create bot and login
 
 bot.on('ready', () => {
@@ -25,13 +29,13 @@ bot.on('ready', () => {
     setupWebServers()
 })
 
-bot.on('message', msg => {
+bot.on('messageCreate', msg => {
     // When message is incoming, send to handler
     if (!msg.author.bot) {
         // Run normal DSF functions with users
         messageHandlers.forEach(x => x(msg, msg.member === null))
     } else if (msg.author.username !== 'DSF Bot') {
         // Small chance of easter egg with other discord bots
-        if (randomNumber(150) === 5) msg.reply('Shut up- bots are not meant to speak in this channel.')
+        if (randomNumber(1500) === 5) msg.reply('Shut up- bots are not meant to speak in this channel.')
     }
 })
