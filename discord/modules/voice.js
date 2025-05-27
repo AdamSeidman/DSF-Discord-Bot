b15df3b330de6672cd99b3d1c4fb98ab47c783a4
Adam Seidman <adamjseidman@gmail.com>
2025-05-27 14:12:23 -0400

    Add untested voice code

--- discord/modules/voice.js
+++ discord/modules/voice.js
@@ -1,163 +1,187 @@
-const fs = require('fs') // TODO Move everything to supabase (for all kinds of assets)
+const path = require('path')
+
 const logger = require('../../utils/logger')
-const { randomArrayItem, copyObject } = require('../../utils/utils')
+const effects = require('../../db/media/effects')
+const { randomNumber } = require('../../utils/utils')
 const { createAudioResource, createAudioPlayer, NoSubscriberBehavior,
     joinVoiceChannel, AudioPlayerStatus } = require('@discordjs/voice')
 
-const dir = './assets'
-const fxDir = 'sound-effects/'
-const ext = '.mp3'
+const MIN_SILENCE_SECONDS = 60 * 5
+const MAX_SILENCE_SECONDS = 60 * 15
 
-let servers = {}
-let effectNames = []
+const MUSIC_ASSET = path.join(__dirname, '../../assets', 'music.mp3')
+const SILENCE_ASSET = path.join(__dirname, '../../assets', 'silence.mp3')
 
-fs.readdir(`${dir}${fxDir}`, (_, files) => {
-    if (files.length < 1) {
-        logger.error('No effect files!', `${dir}${fxDir}`)
-    }
-    files.forEach((file) => {
-        if (file.toLowerCase().endsWith(ext)) {
-            effectNames.push(file.toLowerCase().substring(0, file.length - ext.length))
-        }
-    })
-})
+const guilds = {}
 
-// Exported function to play music
-function playMusic(msg, song, isEffect, keepAlive, isLoud) {
-    if (!song) {
-        song = randomArrayItem(effectNames)
-        if (!isLoud) {
-            logger.info(`Playing 'silence' sound effect [${song}].`, { keepAlive })
+function getGuild(msg, createIfUnavailable=false) {
+    if (!guilds[msg.guild.id]) {
+        if (!createIfUnavailable) return
+        guilds[msg.guild.id] = {
+            guildId: msg.guild.id,
+            channelId: msg.member.voice.channel.id,
+            player: null,
+            connection: null,
+            paused: false,
+            persistant: false,
+            periodic: false,
+            isMusic: false,
+            originalMsg: msg,
+            timeoutId: -1,
         }
     }
-    if (typeof msg !== 'object') {
-        msg = servers[`#${msg}`].msg
-        if (!msg) {
-            logger.warn('Invalid guildId provided for replaying music.', msg)
-            return
-        }
-    }
-    if (msg.member.voice.channel) {
-        if (song === undefined || (isEffect && !effectNames.includes(song))) {
-            logger.warn('Non-existant effect requested.', song)
-            return
-        }
+    return guilds[msg.guild.id]
+}
 
-        // Object to play an audio resource
-        const player = createAudioPlayer({
+function userInVoice(msg) {
+    return !!msg.member?.voice.channel
+}
+
+function playResource(msg, args={}) {
+    if (!userInVoice(msg)) return false
+
+    const guild = getGuild(msg, true)
+    if (guild === undefined) return false
+
+    if (!guild.player || !guild.connection || !guild.channelId === msg.channel.id) {
+        guild.player = createAudioPlayer({
             behaviors: {
                 noSubscriber: NoSubscriberBehavior.Pause
             }
         })
-
-        // Connection to specific channel in guild
-        const connection = joinVoiceChannel({
-            channelId: msg.member.voice.channel.id,
-            guildId: msg.guildId,
+        guild.connection = joinVoiceChannel({
+            channelId: guild.channelId,
+            guildId: guild.guildId,
             adapterCreator: msg.channel.guild.voiceAdapterCreator,
             selfDeaf: false
         })
 
-        // Store locally
-        servers[`#${msg.guildId}`] = {
-            msg,
-            isLoud,
-            player,
-            connection,
-            paused: false,
-            keepAlive: false || keepAlive,
-        }
+        guild.player.on(AudioPlayerStatus.Idle, () => {
+            cancelSchedule(guild)
+            if (guild.paused) return
 
-        // Create new resource from file
-        player.play(createAudioResource(`${dir}${isEffect? fxDir : ''}${song}${ext}`))
-        connection.subscribe(player)
-
-        player.on(AudioPlayerStatus.Idle, () => {
-            let server = servers[`#${msg.guildId}`]
-
-            if (!server.paused) {
-                if (server.isLoud) {
-                    setTimeout(() => playMusic(msg, undefined, true, false, true), 1500)
-                } else if (server.keepAlive) {
-                    // If supposed to stay alive, pause
-                    server.paused = true
-                    server.player.pause()
+            if (guild.persistant) {
+                if (guild.periodic) {
+                    scheduleRandomEffect(guild)
                 } else {
-                    // Only stop if unpaused
-                    connection.destroy()
-                    delete servers[`#${msg.guildId}`]
+                    guild.player.play(effects.getRandomEffect())
                 }
+            } else {
+                guild.connection.destroy()
+                delete guilds[guild.guildId]
             }
         })
-
-        player.on('error', (err) => {
-            logger.error('Error with voice player.', err)
+        guild.player.on('error', (error) => {
+            logger.error('Error with voice player.', error)
         })
-        return true
-    } else if (!isEffect) {
-        msg.channel.send('You aren\'t in a voice channel.')
     }
-}
+    cancelSchedule(guild)
 
-function stopMusic(msg) {
-    let server = servers[`#${msg.guildId}`]
-    if (server) {
-        server.player.stop()
-        server.connection.destroy()
-        delete servers[`#${msg.guildId}`]
+    if (typeof args.effect === 'string') {
+        guild.music = false
+        guild.persistant = false
+        const effect = effects.getEffect(args.effect)
+        if (!effect) return false
+        guild.player.play(effect)
+    } else if (args.continuous) {
+        guild.persistant = true
+        guild.isMusic = false
+        guild.periodic = !!args.periodic
+        guild.player.play(createAudioResource(SILENCE_ASSET))
     } else {
-        msg.channel.send('No music is playing.')
+        guild.persistant = false
+        guild.isMusic = true
+        guild.player.play(createAudioPlayer(MUSIC_ASSET))
     }
+    guild.connection.subscribe(guild.player)
+    return true
 }
 
-function pauseMusic(msg) {
-    if (msg.member.voice.channel) {
-        let server = servers[`#${msg.guildId}`]
+function cancelSchedule(guild) {
+    if (!guild || (guild.timeoutId || -1) < 0) return
 
-        if (server === undefined || server.paused) {
-            msg.channel.send('There is no music playing.')
-        } else {
-            server.paused = true
-            server.player.pause()
-        }
-    } else {
-        msg.channel.send('You aren\'t in a voice channel.')
-    }
+    clearTimeout(guild.timeoutId)
+    guild.timeoutId = -1
 }
 
-function resumeMusic(msg) {
-    if (msg.member.voice.channel) {
-        let server = servers[`#${msg.guildId}`]
-        if (server === undefined || !server.paused) {
-            msg.channel.send('There is no music that is paused.')
-        } else {
-            server.paused = false
-            server.player.unpause()
-        }
-    } else {
-        msg.channel.send('You aren\'t in a voice channel.')
-    }
+function scheduleRandomEffect(guild) {
+    if (!guild?.player) return
+    cancelSchedule(guild)
+
+    guild.timeoutId = setTimeout(() => {
+        guild.player.play(effects.getRandomEffect())
+    }, (randomNumber(MAX_SILENCE_SECONDS - MIN_SILENCE_SECONDS) + MIN_SILENCE_SECONDS) * 1000)
 }
 
-function getKeepAliveIds() {
-    let results = []
-    Object.keys(servers).forEach((id) => {
-        if (servers[id].keepAlive) {
-            results.push(servers[id].msg.guildId)
-        }
+function startSilence(msg) {
+    return playResource(msg, { periodic: true, continuous: true })
+}
+
+function startLoud(msg) {
+    return playResource(msg, { periodic: false, continuous: true })
+}
+
+function playEffect(msg, effect) {
+    return playResource(msg, { effect })
+}
+
+function playMusic(msg) {
+    return playResource(msg)
+}
+
+function pause(msg) {
+    if (!userInVoice(msg)) return false
+
+    const guild = getGuild(msg)
+    if (guild === undefined || guild.paused || !guild.player) return false
+
+    cancelSchedule(guild)
+    guild.paused = true
+    guild.player.pause()
+    return true
+}
+
+function resume(msg) {
+    if (!userInVoice(msg)) return false
+
+    const guild = getGuild(msg)
+    if (guild === undefined || !guild.paused || !guild.player) return false
+
+    guild.paused = false
+    guild.player.unpause()
+
+    if (guild.persistant) {
+        scheduleRandomEffect(guild)
+    }
+    return true
+}
+
+function stop(msg) {
+    const guild = getGuild(msg)
+    if (guild === undefined) return false
+
+    guild.player?.stop()
+    guild.connection?.destroy()
+    delete guilds[guild.guidId]
+    return true
+}
+
+function stopAll() {
+    Object.values(guilds).forEach((guild) => {
+        guild.player?.stop()
+        guild.connection?.destroy()
+        delete guild[guild.guildId]
     })
-    return results
-}
-
-function endAll() {
-    Object.keys(servers).forEach(x => stopMusic({ guildId: x.slice(1) }))
+    return true
 }
 
 module.exports = {
+    startSilence,
+    startLoud,
+    playEffect,
     playMusic,
-    pauseMusic,
-    resumeMusic,
-    endAll,
-    effects: copyObject(effectNames),
-    getKeepAliveIds
+    pause,
+    resume,
+    stop,
+    stopAll
 }
