fc0b3669e40f95378a97c4275db5bfaab9fdf610
Adam Seidman <adamjseidman@gmail.com>
2025-05-29 08:43:56 -0400

    Clean up app index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-require('dotenv').config()
-
 const app = () => {
+    require('dotenv').config()
     require('./utils/logger').init('DSF Bot', 'dsf_')
     const db = require('./db/database')
     require('./web/server')
@@ -8,4 +7,9 @@ const app = () => {
     db.init()
 }
 
-app()
+try {
+    app()
+} catch (error) {
+    console.error('Error initializing DSF!', error)
+    process.exit(1)
+}
