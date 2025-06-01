0f7136149e51fb6a530d3e9e1ebd4c335966f38e
Adam Seidman <adamjseidman@gmail.com>
2025-06-01 17:25:42 -0400

    Missing file

--- discord/commands/fact-check.js
+++ discord/commands/fact-check.js
@@ -1,10 +1,51 @@
-module.exports = { // TODO
-    response: (msg, params) => {
-        if (params.isTestingGuild) {
-            // TODO
-        } else if (!params.injected) {
-            msg.reply({content: 'Unknown Error.', ephemeral: true})
+const logger = require("@adamseidman/logger")
+const { parseFactTemplate, parseLieTemplate } = require("../../fact/construction")
+
+const NUM_EXAMPLES = 10
+
+function parseInput(msg, params, template) {
+    let print = (x) => msg.reply(x)
+    if (params.injected) {
+        print = (x) => msg.channel.send(x)
+    }
+    template = template.trim()
+    if (template.length < 1 || !template.startsWith('[') || !template.endsWith(']')) {
+        print('You need to send a fact template.')
+        return
+    }
+    logger.info('Received new fact template.', template)
+    const facts = []
+    const lies = []
+    for (let i = 0; i < NUM_EXAMPLES; i++) {
+        facts.push(parseFactTemplate(template))
+        lies.push(parseLieTemplate(template))
+    }
+    let builder = [
+        `Provided template:\n> ${template}\n\nTen sample facts:\n`,
+        ...facts.map(x => `> ${x}\n`),
+        '\nTen sample lies:\n',
+        ...lies.map(x => `> ${x}\n`)
+    ]
+    let out = []
+    while (builder.length > 0) {
+        if (out.join('').concat(builder[0]).length >= 2000) {
+            print(out.join(''))
+            out = []
         }
+        out.push(builder.shift())
+    }
+    print(out.join(''))
+}
+
+module.exports = {
+    response: (msg, params) => {
+        let template = ''
+        if (params.injected) {
+            template = params.params.join(' ')
+        } else {
+            template = msg.options.getString('template') || ''
+        }
+        parseInput(msg, params, template)
     },
     argModifier: (builder) => {
         builder.addStringOption((option) => 
@@ -14,5 +55,7 @@ module.exports = { // TODO
                 .setRequired(true)
         )
     },
-    isTesterCommand: true
+    isTesterCommand: true,
+    altMsg: 'Check a fact template.',
+    parseInput
 }
