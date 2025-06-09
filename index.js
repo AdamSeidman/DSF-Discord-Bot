77b44e764e3cdb893f4b7efa894cd1de2989667e
Adam Seidman <adamjseidman@gmail.com>
2025-06-09 10:55:05 -0400

    Gave into peer pressure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { postpone } = require("logic-kit")
 
 const app = (config) => {
+    require('./assets/loadBearingRaccoon').check()
     require("dotenv").config()
     process.dsf = config || require("./config.json")
     require("@adamseidman/logger").init('DSF Bot', 'dsf_', 'yellow')
