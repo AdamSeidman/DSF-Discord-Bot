95e6d40d26036792ed79bcbe07027d645ef552a6
Adam Seidman <adamjseidman@gmail.com>
2025-09-11 16:19:44 -0400

    Separate message handlers and add city Fns

--- discord/modules/messages.js
+++ discord/modules/messages.js
@@ -1,19 +1,11 @@
 const fs = require("fs")
 const path = require("path")
-const voice = require("./voice")
 const Discord = require("discord.js")
 const commands = require("./commands")
 const hosts = require("@tables/hosts")
-const stats = require("@tables/stats")
-const fact = require("../commands/fact")
-const effects = require("@media/effects")
-const phrases = require("@tables/phrases")
 const logger = require("@adamseidman/logger")
-const adjectives = require("@tables/adjectives")
-const construction = require("@facts/construction")
-const effectsGuilds = require("@tables/effectsGuilds")
-const { copyObject, stripPunctuation, removeSpaces, toParts,
-    probabilityCheck, matchesDiscordId} = require("logic-kit")
+const { copyObject, toParts, probabilityCheck } = require("logic-kit")
+const { messageHandlers: extraHandlers } = require("./extraMessageFns")
 
 const COMMAND_PREFIX = global.DEBUG? 'd!' : 'dsf!'
 const availableCommands = []
@@ -44,112 +36,6 @@ function handleCommand(msg) {
         .catch(logger.error)
 }
 
-const pleaseMap = {
-    fact: 'fact',
-    lie: 'lie',
-    prius: 'prius',
-    acronym: 'dsf',
-    gibberish: 'gibberish'
-}
-
-function handlePlease(msg) {
-    let parts = removeSpaces(stripPunctuation(msg.content)).toLowerCase().split('please')
-    parts.pop()
-    if (parts.length < 1) return
-    const [_, pleaseFn] = Object.entries(pleaseMap).find(([key]) => parts.find(x => x.endsWith(key))) || []
-    if (typeof pleaseFn === 'string') {
-        msg.commandName = pleaseFn
-        msg.userParams = {
-            injected: true,
-            isPlease: true,
-            isDM: msg.channel.type === Discord.ChannelType.DM,
-            isTestingGuild: msg.guild?.id == process.env.DISCORD_TESTING_GUILD_ID,
-            params: []
-        }
-        commands.handleSlashCommand(msg)
-            .catch(logger.error)
-    }
-}
-
-function getFindRequestPhrase(message) {
-    message = toParts(message.toLowerCase())
-    const result = {
-        hasPhrase: false,
-        isMe: true,
-        user: '',
-        isFact: null,
-        factPhrase: message[3],
-        subject: ''
-    }
-    if (message.length < 6) {
-        return result
-    }
-    if (message[0] !== 'give' && message[0] !== 'tell') {
-        return result
-    }
-    const userId = matchesDiscordId(message[1])
-    if (userId) {
-        result.user = Discord.userMention(userId) + '\n',
-        result.isMe = false
-    } else if (message[1] !== 'me') {
-        return result
-    }
-    if (message[2] !== 'a' || message[4] !== 'about') {
-        return result
-    }
-    if (message[3] === 'fact' || message[3] === 'lie') {
-        result.isFact = (message[3] === 'fact')
-        result.hasPhrase = true
-    }
-    result.subject = message.slice(5).join(' ')
-    return result
-}
-
-function handlePhrase(msg) {
-    const input = stripPunctuation(msg.content.toLowerCase())
-    const phrase = phrases.getPhrase(removeSpaces(input))
-    const adjective = adjectives.getAll().find(x => toParts(input).includes(x))
-    const findResult = getFindRequestPhrase(input)
-
-    if (findResult.hasPhrase) {
-        const result = construction.findSpecificTemplate(findResult.subject, findResult.isFact)
-        if (result.found) {
-            if (findResult.isMe) {
-                return msg.reply(result.fact)
-            } else {
-                return msg.channel.send(`${findResult.user}${result.fact}`)
-            }
-        } else {
-            return msg.channel.send(`${findResult.user}I could not find a ${findResult.factPhrase} for "${
-                findResult.subject}" in my database.\nHere's a ${findResult.factPhrase} about ${
-                result.alternateSubject} instead:\n${result.fact}`)
-        }
-    } else if (phrase) {
-        if (phrase.is_reply) {
-            return msg.reply(phrase.response)
-        } else {
-            return msg.channel.send(phrase.response)
-        }
-    } else if (adjective) {
-        msg.reply = (content) => {
-            return msg.channel.send(`Did someone say ${
-                adjective}?\nThis calls for a fact!\nReady? Here it is:\n${
-                content}`)
-        }
-        fact.response(msg, { injected: false, user: msg.member })
-    }
-}
-
-function handleSoundEffect(msg) {
-    if (msg.member === null || !effectsGuilds.hasGuild(msg.guild.id)) return
-    const message = removeSpaces(stripPunctuation(msg.content.toLowerCase()))
-    const effect = effects.getList().find(x => message.includes(x))
-    if (effect) {
-        if (voice.playEffect(msg, effect)) {
-            stats.updateStat(msg.member, 'effect')
-        }
-    }
-}
 
 function handleHostMessage(msg) {
     if (!!msg.member && hosts.isHostMessage(msg) && probabilityCheck(global.dsf.hostReactionFrequency)) {
@@ -169,11 +55,9 @@ function handleMentions(msg) {
 module.exports = {
     messageHandlers: [
         handleCommand,
-        handlePlease,
-        handlePhrase,
-        handleSoundEffect,
         handleHostMessage,
-        handleMentions
+        handleMentions,
+        ...extraHandlers
     ],
     cmdPrefix: COMMAND_PREFIX
 }
